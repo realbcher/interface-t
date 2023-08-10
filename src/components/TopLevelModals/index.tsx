42e9ca2a0d03d6b1838d2e503cf9f804560a5f74
realbcher <reacrypt@protonmail.com>
2023-08-10 14:57:09 -0300

    delete uniswapwallet and add smartbch logo as .png

--- src/components/TopLevelModals/index.tsx
+++ src/components/TopLevelModals/index.tsx
@@ -1,7 +1,6 @@
 import { useWeb3React } from '@web3-react/core'
 import { OffchainActivityModal } from 'components/AccountDrawer/MiniPortfolio/Activity/OffchainActivityModal'
 import UniwalletModal from 'components/AccountDrawer/UniwalletModal'
-import UniswapWalletBanner from 'components/Banner/UniswapWalletBanner'
 import AddressClaimModal from 'components/claim/AddressClaimModal'
 import ConnectedAccountBlocked from 'components/ConnectedAccountBlocked'
 import FiatOnrampModal from 'components/FiatOnrampModal'
@@ -28,7 +27,6 @@ export default function TopLevelModals() {
       <ConnectedAccountBlocked account={account} isOpen={accountBlocked} />
       <Bag />
       <UniwalletModal />
-      <UniswapWalletBanner />
       <OffchainActivityModal />
       <TransactionCompleteModal />
       <AirdropModal />
